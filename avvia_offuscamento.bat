eebe7a36d2d0aa5ecda49f479eded905d675cb18
00gianky00 <lavoro.allegretti@gmail.com>
2025-11-13 13:45:00 +0100

    add

--- avvia_offuscamento.bat
+++ avvia_offuscamento.bat
@@ -4,5 +4,5 @@ REM Cambia la directory corrente in quella in cui si trova questo file .bat
 cd /d %~dp0
 REM Esegue lo script Python dell'interfaccia grafica
 echo Avvio di License Manager...
-python.exe src\license_manager.py
+python.exe src\obfuscator_gui.py
 endlocal
